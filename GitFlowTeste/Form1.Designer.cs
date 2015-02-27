bbcd9923b686c22484c183a2eaaf221c141e09e2
unknown <fabio.baia@mga-baia.benner.com.br>
2015-02-27 10:25:20 -0300

    Alterando tamanho dos botoes

--- GitFlowTeste/Form1.Designer.cs
+++ GitFlowTeste/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(37, 57);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(211, 23);
+            this.button1.Size = new System.Drawing.Size(211, 62);
             this.button1.TabIndex = 0;
             this.button1.Text = "Clique para imprimir a saudação";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,7 +46,7 @@
             // 
             this.button2.Location = new System.Drawing.Point(37, 125);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(211, 23);
+            this.button2.Size = new System.Drawing.Size(211, 63);
             this.button2.TabIndex = 1;
             this.button2.Text = "Clique para imprimir a despedida";
             this.button2.UseVisualStyleBackColor = true;
