9927bdc7fcc4b6a70af7e3b977a42305c4ef8292
unknown <fabio.baia@mga-baia.benner.com.br>
2015-02-27 10:20:40 -0300

    Alterando texto do botão

--- GitFlowTeste/Form1.Designer.cs
+++ GitFlowTeste/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(122, 130);
+            this.button1.Location = new System.Drawing.Point(38, 120);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(211, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Clique para imprimir a saudação";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
